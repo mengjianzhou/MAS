e4978343b637e5240b050415bac1dbb5941b3fe7
mengjianzhou <362601125@qq.com>
2018-06-17 09:03:25 +0800

    添加缓存类

--- src/main/java/com/mjz/MASApplication.java
+++ src/main/java/com/mjz/MASApplication.java
@@ -3,6 +3,7 @@ package com.mjz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
@@ -11,6 +12,7 @@ import java.util.Locale;
 
 @MapperScan(value="com.mjz.mapper")
 @SpringBootApplication
+@EnableCaching
 public class MASApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mjz/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.mjz.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        template.setConnectionFactory(factory);
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisTemplate<String,String> redisTemplate){
+        RedisCacheManager cacheManager = new RedisCacheManager(
+                RedisCacheWriter.lockingRedisCacheWriter(redisTemplate.getConnectionFactory()),
+                RedisCacheConfiguration.defaultCacheConfig(),
+                true);
+        return cacheManager;
+    }
+
+}
