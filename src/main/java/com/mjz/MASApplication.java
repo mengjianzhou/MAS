2471c250c69e510b6d81d778994c435ef0767089
mengjianzhou <362601125@qq.com>
2018-06-26 20:54:26 +0800

    清楚dubbo相关依赖

--- src/main/java/com/mjz/MASApplication.java
+++ src/main/java/com/mjz/MASApplication.java
@@ -1,24 +1,21 @@
 package com.mjz;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
 
-import java.util.Locale;
 
 @MapperScan(value="com.mjz.mapper")
 @SpringBootApplication
-@EnableDubboConfiguration
 @EnableCaching
 public class MASApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MASApplication.class, args);
+        SpringApplication app = new SpringApplication(MASApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
--- src/main/java/com/mjz/config/RedisConfig.java
+++ src/main/java/com/mjz/config/RedisConfig.java
@@ -1,9 +1,6 @@
 package com.mjz.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
+
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -15,7 +12,6 @@ import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import java.lang.reflect.Method;
 
